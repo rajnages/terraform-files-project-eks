f143f0b64fca91c4e8f53d8545f6522a9baa1299
DevOps-AWS-123 <nagesrajavarapu@gmail.com>
2024-11-09 18:15:20 +0530

    added some changes

--- terraform/install.sh
+++ terraform/install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Exit on any error
-set -e
+#set -e
 
 echo "Starting installation process..."
 
@@ -11,22 +11,14 @@ sudo apt-get update -y
 sudo apt-get upgrade -y
 
 # Install Java
-echo "Installing Java..."
-sudo mkdir -p /etc/apt/keyrings
-wget -O - https://packages.adoptium.net/artifactory/api/gpg/key/public | sudo tee /etc/apt/keyrings/adoptium.asc
-echo "deb [signed-by=/etc/apt/keyrings/adoptium.asc] https://packages.adoptium.net/artifactory/deb $(awk -F= '/^VERSION_CODENAME/{print$2}' /etc/os-release) main" | sudo tee /etc/apt/sources.list.d/adoptium.list
-sudo apt-get update -y
-sudo apt-get install temurin-17-jdk -y
-/usr/bin/java --version
-
-# Install Jenkins
-echo "Installing Jenkins..."
-curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | sudo tee /usr/share/keyrings/jenkins-keyring.asc > /dev/null
-echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] https://pkg.jenkins.io/debian-stable binary/ | sudo tee /etc/apt/sources.list.d/jenkins.list > /dev/null
-sudo apt-get update -y
-sudo apt-get install jenkins -y
-sudo systemctl enable jenkins
-sudo systemctl start jenkins
+sudo wget -O /usr/share/keyrings/jenkins-keyring.asc \
+    https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key
+sudo echo "deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc]" \
+    https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+    /etc/apt/sources.list.d/jenkins.list > /dev/null
+sudo apt-get update
+sudo apt-get install fontconfig openjdk-17-jre
+sudo apt-get install jenkins
 
 # Install Docker
 echo "Installing Docker..."
